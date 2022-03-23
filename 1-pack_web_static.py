fb6d0748b43f35b640e57467c17bf33c4e2a3900
Timi-T <timiopebode@gmail.com>
2022-03-23 16:43:21 +0000

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+fabfile to create an archive
+"""
+
+from fabric.api import local, settings
+from datetime import datetime
+from __future__ import with_statement
+
+
+def do_pack():
+    """function to compress a directory"""
+
+    local("mkdir -p versions")
+    current_time = str(datetime.now())
+    current_time = (current_time.split('.'))[0]
+    current_time = current_time.replace(':', '')
+    current_time = current_time.replace(' ', '')
+    current_time = current_time.replace('-', '')
+    file_path = 'versions/web_static_' + current_time + '.tgz'
+    with settings(warn_only=True):
+        result = local("tar -zcvf {} web_static".format(file_path))
+    if result.failed:
+        return None
+    else:
+        return file_path
