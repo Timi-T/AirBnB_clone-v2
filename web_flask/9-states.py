7865443412107c3c9f91d64e8f998df97b695596
Timi-T <timiopebode@gmail.com>
2022-04-08 21:14:53 +0000

    2 app routes, 1 function

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -14,42 +14,40 @@ app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-def states():
-    """List all the states in the database"""
-    all_states = storage.all(State)
-    dict_states = {}
-    for key, val in all_states.items():
-        st_key = val.id
-        st_val = val.name
-        dict_states[st_key] = st_val
-    dict_states = sorted(dict_states.items(), key=lambda x: x[1])
-    return render_template("9-states.html", states=dict_states)
-
-
 @app.route('/states/<id>', strict_slashes=False)
-def cities_in_state(id):
+def cities_in_state(id=None):
     """List all cities in a state"""
     all_states = storage.all(State)
     found = 0
-    for k, val in all_states.items():
-        if val.id == id:
-            state = val
-            st_name = state.name
-            found = 1
-            break
-    if found == 1:
-        if os.getenv('HBNB_TYPE_STORAGE') != 'db':
-            cities = state.cities()
+    if id:
+        for k, val in all_states.items():
+            if val.id == id:
+                state = val
+                st_name = state.name
+                found = 1
+                break
+        if found == 1:
+            if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+                cities = state.cities()
+            else:
+                cities = state.cities
+            ct_dict = {}
+            for city in cities:
+                ct_key = city.id
+                ct_val = city.name
+                ct_dict[ct_key] = ct_val
+            ct_array = sorted(ct_dict.items(), key=lambda x: x[1])
+            return render_template("9-states.html", cities=ct_array, state=st_name)
         else:
-            cities = state.cities
-        ct_dict = {}
-        for city in cities:
-            ct_key = city.id
-            ct_val = city.name
-            ct_dict[ct_key] = ct_val
-        ct_array = sorted(ct_dict.items(), key=lambda x: x[1])
-        return render_template("9-states.html", cities=ct_array, state=st_name)
-    return render_template("9-states.html", not_found='Not found!')
+            return render_template("9-states.html", not_found='Not found!')
+    else:
+        dict_states = {}
+        for key, val in all_states.items():
+            st_key = val.id
+            st_val = val.name
+            dict_states[st_key] = st_val
+        dict_states = sorted(dict_states.items(), key=lambda x: x[1])
+        return render_template("9-states.html", states=dict_states)
 
 
 @app.teardown_appcontext
