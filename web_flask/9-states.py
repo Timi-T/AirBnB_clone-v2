388ec087fd47be13fae9a991371ee6f2b00b3451
Timi-T <timiopebode@gmail.com>
2022-04-08 17:07:29 +0000

    task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+Start airbnb web application with flask
+"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """List all the states in the database"""
+    all_states = storage.all(State)
+    dict_states = {}
+    for key, val in all_states.items():
+        st_key = val.id
+        st_val = val.name
+        dict_states[st_key] = st_val
+    dict_states = sorted(dict_states.items(), key=lambda x: x[1])
+    return render_template("9-states.html", states=dict_states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def cities_in_state(id):
+    """List all cities in a state"""
+    all_states = storage.all(State)
+    found = 0
+    for k, val in all_states.items():
+        if val.id == id:
+            state = val
+            st_name = state.name
+            found = 1
+            break
+    if found == 1:
+        if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+            cities = state.cities()
+            ct_dict = {}
+            for k, v in cities.items():
+                ct_key = v.id
+                ct_val = v.name
+                ct_dict[ct_key] = ct_val
+        else:
+            cities = state.cities
+            ct_dict = {}
+            for city in cities:
+                ct_key = city.id
+                ct_val = city.name
+                ct_dict[ct_key] = ct_val
+        ct_array = sorted(ct_dict.items(), key=lambda x: x[1])
+        return render_template("9-states.html", cities=ct_array, state=st_name)
+    return render_template("9-states.html", not_found='Not found!')
+
+
+@app.teardown_appcontext
+def close_session(response_or_exc):
+    """close sqlalchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
